49aa8ae88fbe121d54888e84658911062198c098
codedecode25 <61463305+codedecode25@users.noreply.github.com>
2023-07-07 20:35:35 +0530

    Dummy commit to show jenkins build

--- src/main/java/com/codeddecode/restaurantlisting/controller/RestaurantController.java
+++ src/main/java/com/codeddecode/restaurantlisting/controller/RestaurantController.java
@@ -18,6 +18,8 @@ public class RestaurantController {
     @Autowired
     RestaurantService restaurantService;
 
+
+    
     @GetMapping("/fetchAllRestaurants")
     public ResponseEntity<List<RestaurantDTO>> fetchAllRestaurants(){
         List<RestaurantDTO> allRestaurants = restaurantService.findAllRestaurants();
